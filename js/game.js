73b50390c82254a94098d5fb1e08e68c72628dbb
Renzil DSouza <renzil@gmail.com>
2022-04-08 03:36:52 +0530

    update_hearts() + code reordering

--- js/game.js
+++ js/game.js
@@ -27,9 +27,10 @@ function on_correct_answer() {
 }
 
 function on_wrong_answer() {
+    decrement_lives();
+    update_hearts();
     highlight_answer_button(clicked_button, "answer_button_wrong");
     after_n_seconds(function() {
-        decrement_lives();
         check_lives(on_game_over, fetch_new_question);
     }, 1);
 }
@@ -87,6 +88,10 @@ function update_level_heading() {
 
 function decrement_lives() {
     // TBD decrement lives by 1
+}
+
+function update_hearts() {
+    // TBD update the hearts to show remaining lives (use ri-heart-3-line class for empty heart)
 }
 
 function check_lives(on_game_over, fetch_new_question) {
